c0c93b65c33d66256ff21b80a571be26c1c27cca
Guilherme <guirafaelrn@gmail.com>
2024-06-11 12:16:58 -0300

    feat: implementacao de registro e login de usuarios com identity

---
+++ src/CrudFornecedores.API/Controllers/AuthController.cs
@@ -0,0 +1,73 @@
+﻿using CrudFornecedores.API.DTO;
+using CrudFornecedores.Domain.Intefaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace CrudFornecedores.API.Controllers
+{
+    [Route("api")]
+    public class AuthController : MainController
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthController(INotificador notificador,
+                               SignInManager<IdentityUser> signInManager,
+                               UserManager<IdentityUser> userManager) : base(notificador)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(RegisterUserDTO registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            /* CRIANDO USUARIO */ 
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return CustomResponse(registerUser);
+            }
+
+            foreach (var error in result.Errors)
+            {
+                NotificarErro(error.Description);
+            }
+
+            return CustomResponse(registerUser);
+        }
+
+        [HttpPost("entrar")]
+        public async Task<ActionResult> Login(LoginUserDTO loginUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+
+            /* FAZENDO LOGIN DO USUARIO COM EMAIL, SENHA, PERSISTENTE E BLOQUEAR POR TENTATIVAS INVALIDAS */
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded) return CustomResponse(loginUser);
+            
+            if(result.IsLockedOut)
+            {
+                NotificarErro("O usuario foi temporariamente bloqueado por tentativas inválidas");
+                return CustomResponse(loginUser);
+            }
+
+            NotificarErro("Usuario ou senha incorretos");
+            return CustomResponse(loginUser);
+        }
+    }
+}
